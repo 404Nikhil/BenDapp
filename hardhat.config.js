9e680c1f0c1f9b94c53d3d8d1145e8f60ad59949
404Nikhil <nicksd111@gmail.com>
2024-07-25 22:40:29 +0530

    contract deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,37 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
 const fs = require("fs");
-const privateKey = fs.readFileSync("secrete.txt").toString();
+const privateKey = fs.readFileSync("secret.txt").toString();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "sepolia",
   networks: {
-    hardhat: {
-      chainId: 4202,
-    },
-    sepolia: {
-      url: "https://rpc.sepolia-api.lisk.com",
-      accounts: [privateKey],
-      gasPrice: 1000000000,
-    },
-    BitTorrent: {
-      url: "https://pre-rpc.bt.io/",
-      accounts: [privateKey],
-      gasPrice: 1000000000,
-    },
+    // hardhat: {
+    //   chainId: 4202,
+    // },
+    // sepolia: {
+    //   url: "https://rpc.sepolia-api.lisk.com",
+    //   accounts: [privateKey],
+    //   gasPrice: 1000000000,
+    // },
+    // BitTorrent: {
+    //   url: "https://pre-rpc.bt.io/",
+    //   accounts: [privateKey],
+    //   gasPrice: 1000000000,
+    // },
     zkEVMCardonaTestnet: {
       url: "https://polygon-zkevm-cardona.blockpi.network/v1/rpc/public",
       accounts: [privateKey],
       gas: 6000000,
       gasPrice: 20000000000,
     },
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [privateKey],
-    },
-    matic: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS",
-      accounts: [privateKey],
-    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.maticvigil.com",
+    //   accounts: [privateKey],
+    // },
+    // matic: {
+    //   url: "https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS",
+    //   accounts: [privateKey],
+    // },
   },
   solidity: "0.8.24",
   allowUnlimitedContractSize: true,
@@ -40,4 +40,5 @@ module.exports = {
   loggingEnabled: true,
 };
 
-// npx hardhat ignition deploy ./ignition/modules/Lock.js --network BitTorrent
+// npx hardhat ignition deploy ./ignition/modules/Lock.js --network zkEVMCardonaTestnet
+// 0xe82a687C79AdB17D89335dc36e481ec9bE7D84c7
